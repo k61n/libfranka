1e669c7849afb972bfb52e3e88e8e07747e181bd
Konstantin Kholostov <k.kholostov@fz-juelich.de>
2026-08-04 16:59:47 +0200

    update eigen3 integration
    Change-Id: Ibb87ed8675cf3e67157da70125ea6756cdc1e7af

--- cmake/FindEigen3.cmake
+++ cmake/FindEigen3.cmake
@@ -4,13 +4,20 @@ mark_as_advanced(FORCE Eigen3_DIR)
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(Eigen3
   FOUND_VAR Eigen3_FOUND
-  REQUIRED_VARS EIGEN3_INCLUDE_DIRS
+  REQUIRED_VARS Eigen3_DIR
+  VERSION_VAR Eigen3_VERSION
 )
 
-if(NOT TARGET Eigen3::Eigen3)
+if(Eigen3_FOUND AND NOT TARGET Eigen3::Eigen3)
   add_library(Eigen3::Eigen3 INTERFACE IMPORTED)
-  set_target_properties(Eigen3::Eigen3 PROPERTIES
-    INTERFACE_INCLUDE_DIRECTORIES ${EIGEN3_INCLUDE_DIRS}
-    INTERFACE_COMPILE_DEFINITIONS "${EIGEN3_DEFINITIONS}"
-  )
+  if(TARGET Eigen3::Eigen)
+    set_target_properties(Eigen3::Eigen3 PROPERTIES
+      INTERFACE_LINK_LIBRARIES Eigen3::Eigen
+    )
+  else()
+    set_target_properties(Eigen3::Eigen3 PROPERTIES
+      INTERFACE_INCLUDE_DIRECTORIES "${EIGEN3_INCLUDE_DIRS}"
+      INTERFACE_COMPILE_DEFINITIONS "${EIGEN3_DEFINITIONS}"
+    )
+  endif()
 endif()
